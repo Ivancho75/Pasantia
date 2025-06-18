c39e616c7dc12d04b4bf45862e6138366c9c55f1
Ivancho75 <ivan.rossi150@gmail.com>
2025-06-18 11:34:55 -0300

    Inicio Pag con tailwind

---
+++ login-tailwind/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-ivan bg bg-amber-500'>Bienvenidos a la página de Ivan</h1>
+      <h2 className='text-black bg-amber-700'>sección de futbol</h2>
+      <h3 className='text-white bg-amber-900'>sección de baloncesto</h3>
+      <h4 className='text-black bg-amber-800'>sección de tenis</h4>
+    </>
+  )
+}
+
+export default App
